aa574bb41eb52fa1286c9c5ad6199af0a80e99ab
long55042 <76759899+long55042@users.noreply.github.com>
2021-01-07 21:19:37 +0700

    Update bai_5.java

--- bai_5.java
+++ bai_5.java
@@ -1,8 +1,6 @@
-
-import static java.lang.Math.sqrt;
 import java.util.Scanner;
 public class bai_5 {
-    kiemtranguyento(int n) {
+   boolean KTSNT (int n) {
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 return false;
@@ -14,10 +12,9 @@ public class bai_5 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         System.out.printf("Kiem tra so nguyen to \n");
-        System.out.printf("Nhap vao so nguyen duong \n");
         int n = s.nextInt();
         bai_5 snt = new bai_5();
-        if (snt.kiemtranguyento(n)) {
+        if (snt.KTSNT(n)) {
             System.out.printf("So : " + n + " la so nguyen to");
         } else {
             System.out.printf("So : " + n + " khong phai la so nguyen to");
